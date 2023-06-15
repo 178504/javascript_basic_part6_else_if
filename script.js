c1ec5169c41da7b3e94bcfea59892d6829f8d454
tsujishouta <koshi51455@gmail.com>
2023-06-16 00:04:10 +0900

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+var text = "検討します";
+if(text == "可能"){
+  console.log("true");
+  console.log(text);
+}else if(text == "不可能"){
+  console.log("false");
+  console.log(text);
+}else{
+  console.log("検討します");
+}
